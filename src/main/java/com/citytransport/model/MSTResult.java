a4d0a0cd9e72c31083f3a25bc0a9ce71db0e3e72
ssagatov <you@example.com>
2025-10-26 03:36:21 -0700

    MSTResult (model)

--- src/main/java/com/citytransport/model/MSTResult.java
+++ src/main/java/com/citytransport/model/MSTResult.java
@@ -1,4 +1,31 @@
 package com.citytransport.model;
 
+import com.citytransport.graph.Edge;
+
+import java.util.List;
+
 public class MSTResult {
+    private final List<Edge> mstEdges;
+    private final int totalCost;
+    private final int originalVertices;
+    private final int originalEdges;
+    private final long operationCount;
+    private final long executionTimeMs;
+
+    public MSTResult(List<Edge> mstEdges, int totalCost, int originalVertices,
+                     int originalEdges, long operationCount, long executionTimeMs) {
+        this.mstEdges = mstEdges;
+        this.totalCost = totalCost;
+        this.originalVertices = originalVertices;
+        this.originalEdges = originalEdges;
+        this.operationCount = operationCount;
+        this.executionTimeMs = executionTimeMs;
+    }
+
+    public List<Edge> getMstEdges() { return mstEdges; }
+    public int getTotalCost() { return totalCost; }
+    public int getOriginalVertices() { return originalVertices; }
+    public int getOriginalEdges() { return originalEdges; }
+    public long getOperationCount() { return operationCount; }
+    public long getExecutionTimeMs() { return executionTimeMs; }
 }
